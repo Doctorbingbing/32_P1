2c2d034b7772c1c4000a1d55e5bb79e72ffbff5d
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2018-07-12 22:41:37 -0700

    Initial Project Pieces
    Setup a little bit of code for the initial bits of the project. Much work to be done

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _32_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+
+             string key;
+             string path;
+             string[] sep;
+             string input;
+             input = Console.ReadLine();
+             sep = Console.ReadLine().Split(' ');
+             key = sep[0].ToString();
+             path = input.Substring(3,input.Length);
+             if (key == "D")   //Directory
+            {
+                // Print the path to directory and 
+                // print all the files and subdirectories 
+                // in that directory
+                Console.WriteLine(path);
+
+            }
+            else if (sep[0] == "R") //Recursive
+            {
+
+            }
+          //  else if
+        
+         //   {
+          //       Console.WriteLine('ERROR')   
+                //Print "ERROR" and read line for input. 
+                //Continue until the input is valid.
+         //   } 
+        }
+    }
+}
