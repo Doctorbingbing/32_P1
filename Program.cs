554d93e770d46f60d82884326c28549d207441a9
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2018-07-16 18:05:06 -0700

    D and R search implementation
    Got the Directory and recursive searches working according to program specifications. Simplified functions and added docstrings to non-intuitive functions.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,54 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 namespace _32_P1
 {
     class Program
     {
+       
+        static List<string> EnumerateContents(string path)
+        {
+
+                List<string> files = new List<string>(Directory.EnumerateFiles(path));
+                List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+            
+                
+                files.AddRange(dirs);
+                return files;
+            
+        }
+        static List<string> FindFiles(string path)
+        {
+            List<string> files = new List<string>(Directory.EnumerateFiles(path));
+            return files;
+        }
+
+        static List<string> FindDirs(string path)
+        {
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+            return dirs;
+        }
+
+        static void RecursiveSearch(List<string> directories)
+        {
+            foreach (var  dir in directories)
+            {
+                List<string> towrite = FindFiles(dir);
+                List<string> subdirectory = FindDirs(dir);
+                foreach (var file in towrite)
+                {
+                    Console.WriteLine(file);
+                }
+                foreach (var subdir in subdirectory)
+                {
+                    RecursiveSearch(subdirectory);
+                }
+                
+            }
+            return;
+        }
+
         static void Main(string[] args)
         {
            // Console.WriteLine("Hello World!");
@@ -12,29 +57,63 @@ namespace _32_P1
              string path;
              string[] sep;
              string input;
+             List<string> files;
+             List<string> dirs;
+             
+            
              input = Console.ReadLine();
-             sep = Console.ReadLine().Split(' ');
+             
+             sep = input.Split(' ');
              key = sep[0].ToString();
-             path = input.Substring(3,input.Length);
+             path = input.Substring(2,input.Length-2);                                          // user input minus the qualifier
              if (key == "D")   //Directory
             {
-                // Print the path to directory and 
-                // print all the files and subdirectories 
-                // in that directory
-                Console.WriteLine(path);
+                files = FindFiles(path);
+                //List<string> files = new List<string>(Directory.EnumerateFiles(path));         
+              //  List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));                                              
+                files.Sort();                                                                   // Sorts the combined list in lexographic order
+                foreach (var item in files) 
+                {
+                    Console.WriteLine(item);                                                    //Prints all the files within the specified path in the now sorted order
+                }
+               
+                string nextinput = Console.ReadLine();
+
 
             }
             else if (sep[0] == "R") //Recursive
             {
+               files = FindFiles(path);
+               dirs = FindDirs(path);
+               foreach (var item in files)
+               {
+                  Console.WriteLine(item);
+               }
 
+               RecursiveSearch(dirs);
+
+              //  List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+               // foreach (var item in dirs)
+                {
+                    
+                }
+                Console.WriteLine(path);
             }
-          //  else if
+
+
+
+            // FUNCTIONS
+           
+             
+
+
+             else
         
-         //   {
-          //       Console.WriteLine('ERROR')   
+           {
+                Console.WriteLine("ERROR");
                 //Print "ERROR" and read line for input. 
                 //Continue until the input is valid.
-         //   } 
+           } 
         }
     }
 }
